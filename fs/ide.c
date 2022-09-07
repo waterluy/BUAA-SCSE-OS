1eac20a6a5bb1ba439613f7a73507056596b9130
wanglu <573253193@qq.com>
2022-09-07 11:56:38 +0800

    1

---
+++ fs/ide.c
@@ -0,0 +1,323 @@
+/*
+ * operations on IDE disk.
+ */
+
+#include "fs.h"
+#include "lib.h"
+#include <mmu.h>
+
+// Overview:
+// 	read data from IDE disk. First issue a read request through
+// 	disk register and then copy data from disk buffer
+// 	(512 bytes, a sector) to destination array.
+//
+// Parameters:
+//	diskno: disk number.
+// 	secno: start sector number.
+// 	dst: destination for data read from IDE disk.
+// 	nsecs: the number of sectors to read.
+//
+// Post-Condition:
+// 	If error occurrs during the read of the IDE disk, user_panic.
+//
+// Hint: use syscalls to access device registers and buffers
+/*** exercise 5.2 ***/
+void
+ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
+{
+	// 0x200: the size of a sector: 512 bytes.
+	u_int offset_begin = secno * 0x200;
+	u_int offset_end = offset_begin + nsecs * 0x200;
+	u_int offset = 0;
+
+	while (offset_begin + offset < offset_end) {
+	    u_int now = offset_begin + offset;
+        if (syscall_write_dev((u_int)(&diskno), (u_int)0x13000010, (u_int)4) < 0) {
+            user_panic("1-syscall_write_dev error!!!\n");
+        }
+        if (syscall_write_dev((u_int)(&now), (u_int)0x13000000, (u_int)4) < 0) {
+            user_panic("2-syscall_write_dev error!!!\n");
+        }
+        int zero = 0;
+        if (syscall_write_dev((u_int)(&zero), (u_int)0x13000020, (u_int)1) < 0) {
+            user_panic("3-syscall_write_dev error!!!\n");
+        }
+        int ret = 0;
+        if (syscall_read_dev((u_int)(&ret), (u_int)0x13000030, (u_int)4) < 0) {
+            user_panic("4-syscall_read_dev error!!!\n");
+        }
+        if (ret != 0) {
+            if (syscall_read_dev((u_int)dst + offset, (u_int)0x13004000, (u_int)512) < 0) {
+                user_panic("4-syscall_read_dev error!!!\n");
+            }
+        } else {
+            user_panic("5- ret ==0\n");
+        }
+        offset = offset + 0x200;
+	}
+}
+
+
+// Overview:
+// 	write data to IDE disk.
+//
+// Parameters:
+//	diskno: disk number.
+//	secno: start sector number.
+// 	src: the source data to write into IDE disk.
+//	nsecs: the number of sectors to write.
+//
+// Post-Condition:
+//	If error occurrs during the read of the IDE disk, user_panic.
+//
+// Hint: use syscalls to access device registers and buffers
+/*** exercise 5.2 ***/
+void
+ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
+{
+	// Your code here
+	u_int offset_begin = secno * 0x200;
+	u_int offset_end = offset_begin + nsecs * 0x200;
+	u_int offset = 0;
+
+	// DO NOT DELETE WRITEF !!!
+	writef("diskno: %d\n", diskno);
+
+	while (offset_begin + offset < offset_end) {
+		//  copy data from source array to disk buffer.
+		// if error occur, then user_panic.
+		u_int now = offset_begin + offset;
+        if (syscall_write_dev((u_int)(&diskno), (u_int)0x13000010, (u_int)4) < 0) { //id
+            user_panic("1-syscall_write_dev error!!!\n");
+        }
+        if (syscall_write_dev((u_int)(&now), (u_int)0x13000000, (u_int)4) < 0) { //offset
+            user_panic("2-syscall_write_dev error!!!\n");
+        }
+        if (syscall_write_dev((u_int)(src + offset), (u_int)0x13004000, (u_int)512) < 0) {
+            user_panic("3-syscall_write_dev error!!!\n");
+        }
+        int one = 1;
+        if (syscall_write_dev((u_int)(&one), (u_int)0x13000020, (u_int)1) < 0) {
+            user_panic("4-syscall_write_dev error!!!\n");
+        }
+        int ret = 0;
+        if (syscall_read_dev((u_int)(&ret), (u_int)0x13000030, (u_int)4) < 0) {
+            user_panic("5-syscall_read_dev error!!!\n");
+        }
+        if (ret == 0) {
+            user_panic("6-write dev error!!!\n");
+        }
+        offset = offset + 0x200;
+	}
+}
+
+int raid4_valid(u_int diskno) {
+    if (syscall_write_dev((u_int)(&diskno), (u_int)0x13000010, (u_int)4) < 0) { //id
+        user_panic("1-syscall_write_dev error!!!\n");
+    }
+    if (syscall_write_dev((u_int)0, (u_int)0x13000000, (u_int)4) < 0) { //offset
+        user_panic("2-syscall_write_dev error!!!\n");
+    }
+    int zero = 0;
+    if (syscall_write_dev((u_int)(&zero), (u_int)0x13000020, (u_int)1) < 0) {
+        user_panic("3-syscall_write_dev error!!!\n");
+    }
+    int ret = 0;
+    if (syscall_read_dev((u_int)(&ret), (u_int)0x13000030, (u_int)4) < 0) {
+        user_panic("4-syscall_read_dev error!!!\n");
+    }
+    if (ret != 0) { //valid
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int raid4_write(u_int blockno, void *src) {
+    int i, j;
+    u_int p[BY2SECT / 4];
+    int temp[10];
+    int wrong[10];
+    int destroy = 0;
+    u_int offset = 0;
+    void *now;
+
+    for (i = 0; i < (BY2SECT / 4); ++i) {
+        p[i] = 0;
+    }
+
+    int secno = blockno * 2;
+    for (i = 1; i <= 4; ++i) {
+        wrong[i] = raid4_valid(i);
+        now = src + offset;
+        if (wrong[i] == 0) {
+            destroy++;
+        } else {
+            ide_write((u_int)i, secno, now, 1);
+        }
+        for (j = 0; j < (BY2SECT / 4); ++j) {
+            u_int *num = ((u_int *)now);
+            p[j] = p[j] ^ num[j];
+        }
+        offset = offset + BY2SECT;
+    }
+    wrong[5] = raid4_valid(5);
+    if (wrong[5] == 0) {
+        destroy++;
+    } else {
+        ide_write(5, secno, (void *)p, 1);
+    }
+
+    for (i = 0; i < (BY2SECT / 4); ++i) {
+        p[i] = 0;
+    }
+
+    secno = blockno * 2 + 1;
+    for (i = 1; i <= 4; ++i) {
+        now = src + offset;
+        if (wrong[i] != 0) {
+            ide_write((u_int)i, secno, now, 1);
+        }
+        u_int *num = ((u_int *)now);
+        for (j = 0; j < (BY2SECT / 4); ++j) {
+            p[j] = p[j] ^ num[j];
+        }
+        offset = offset + BY2SECT;
+    }
+    if (wrong[5] != 0) {
+        ide_write(5, secno, (void *)p, 1);
+    }
+    return destroy;
+}
+
+int raid4_read(u_int blockno, void *dst) {
+    int destroy = 0;
+    int wrong[10];
+    int wrong_disk_no;
+    int i, j;
+    for (i = 1; i <= 5; ++i) {
+        wrong[i] = raid4_valid(i);
+        if (wrong[i] == 0) {
+            destroy++;
+            wrong_disk_no = i;
+        }
+    }
+    if (destroy > 1) {
+        return destroy;
+    }
+    u_int offset;
+    int secno;
+    void *now;
+    if ((destroy == 1) && (wrong[5]) == 0) {
+        offset = 0;
+        secno = blockno * 2;
+        for (i = 1; i <= 4; ++i) {
+            now = dst + offset;
+            ide_read(i, secno, now, 1);
+            offset = offset + BY2SECT;
+        }
+        secno = blockno * 2 + 1;
+        for (i = 1; i <= 4; ++i) {
+            now = dst + offset;
+            ide_read(i, secno, now, 1);
+            offset = offset + BY2SECT;
+        }
+        return 1;
+    }
+    if ((destroy == 1) && (wrong[5] == 1)) {
+
+        return 1;
+    }
+    // destroy == 0;
+    int ret = 0;
+    offset = 0;
+    u_int p[BY2SECT / 4];
+    u_int pca[BY2SECT / 4];
+    secno = blockno * 2;
+    ide_read(5, secno, (void *)p, 1);
+    for (i = 0; i < (BY2SECT / 4); ++i) {
+        pca[i] = 0;
+    }
+    for (i = 1; i <= 4; ++i) {
+        now = dst + offset;
+        ide_read(i, secno, now, 1);
+        u_int *num = ((u_int *)now);
+        for (j = 0; j < (BY2SECT / 4); ++j) {
+            pca[j] = pca[j] ^ num[j];
+        }
+        offset = offset + BY2SECT;
+    }
+    for (j = 0; j < (BY2SECT / 4); ++j) {
+        if (p[j] != pca[j]) {
+            ret = -1;
+            break;
+        }
+    }
+    secno = blockno * 2 + 1;
+    ide_read(5, secno, (void *)p, 1);
+    for (i = 0; i < (BY2SECT / 4); ++i) {
+        pca[i] = 0;
+    }
+    for (i = 1; i <= 4; ++i) {
+        now = dst + offset;
+        ide_read(i, secno, now, 1);
+        u_int *num = ((u_int *)now);
+        for (j = 0; j < (BY2SECT / 4); ++j) {
+            pca[j] = pca[j] ^ num[j];
+        }
+        offset = offset + BY2SECT;
+    }
+    if (ret != -1) {
+        for (j = 0; j < (BY2SECT / 4); ++j) {
+            if (p[j] != pca[j]) {
+                ret = -1;
+                break;
+            }
+        }
+    }
+    return ret;
+}
+
+void raid0_write(u_int secno, void *src, u_int nsecs) {
+    u_int offset_begin = secno * 0x200;
+    u_int offset_end = offset_begin + nsecs * 0x200;
+    u_int offset = 0;
+    int nowsec = secno;
+    while (offset_begin + offset < offset_end) {
+        if ((nowsec % 2) == 0) {
+            ide_write(1, (u_int)(nowsec / 2), (src + offset), 1);
+        } else {
+            ide_write(2, (u_int)(nowsec / 2), (src + offset), 1);
+        }
+        offset = offset + 0x200;
+        nowsec = nowsec + 1;
+    }
+}
+
+void raid0_read(u_int secno, void *dst, u_int nsecs) {
+    u_int offset_begin = secno * 0x200;
+    u_int offset_end = offset_begin + nsecs * 0x200;
+    u_int offset = 0;
+    int nowsec = secno;
+    while (offset_begin + offset < offset_end) {
+        if ((nowsec % 2) == 0) {
+            ide_read(1, (u_int)(nowsec / 2), (dst + offset), 1);
+        } else {
+            ide_read(2, (u_int)(nowsec / 2), (dst + offset), 1);
+        }
+        offset = offset + 0x200;
+        nowsec = nowsec + 1;
+    }
+}
+
+int time_read() {
+    int time = 0;
+    int upd = 1;
+    if (syscall_write_dev((u_int)&upd, (u_int)0x15000000, 4) < 0) {
+        return -1;
+    }
+    if (syscall_read_dev((u_int)&time, (u_int)0x15000010, 4) < 0) {
+        return -1;
+    }
+    return time;
+}
---
+++ user/fork.c
@@ -0,0 +1,228 @@
+// implement fork from user space
+
+#include "lib.h"
+#include <mmu.h>
+#include <env.h>
+
+
+/* ----------------- help functions ---------------- */
+
+/* Overview:
+ * 	Copy `len` bytes from `src` to `dst`.
+ *
+ * Pre-Condition:
+ * 	`src` and `dst` can't be NULL. Also, the `src` area
+ * 	 shouldn't overlap the `dest`, otherwise the behavior of this
+ * 	 function is undefined.
+ */
+void user_bcopy(const void *src, void *dst, size_t len)
+{
+	void *max;
+
+	//	writef("~~~~~~~~~~~~~~~~ src:%x dst:%x len:%x\n",(int)src,(int)dst,len);
+	max = dst + len;
+
+	// copy machine words while possible
+	if (((int)src % 4 == 0) && ((int)dst % 4 == 0)) {
+		while (dst + 3 < max) {
+			*(int *)dst = *(int *)src;
+			dst += 4;
+			src += 4;
+		}
+	}
+
+	// finish remaining 0-3 bytes
+	while (dst < max) {
+		*(char *)dst = *(char *)src;
+		dst += 1;
+		src += 1;
+	}
+
+	//for(;;);
+}
+
+/* Overview:
+ * 	Sets the first n bytes of the block of memory
+ * pointed by `v` to zero.
+ *
+ * Pre-Condition:
+ * 	`v` must be valid.
+ *
+ * Post-Condition:
+ * 	the content of the space(from `v` to `v`+ n)
+ * will be set to zero.
+ */
+void user_bzero(void *v, u_int n)
+{
+	char *p;
+	int m;
+
+	p = v;
+	m = n;
+
+	while (--m >= 0) {
+		*p++ = 0;
+	}
+}
+/*--------------------------------------------------------------*/
+
+/* Overview:
+ * 	Custom page fault handler - if faulting page is copy-on-write,
+ * map in our own private writable copy.
+ *
+ * Pre-Condition:
+ * 	`va` is the address which leads to a TLBS exception.
+ *
+ * Post-Condition:
+ *  Launch a user_panic if `va` is not a copy-on-write page.
+ * Otherwise, this handler should map a private writable copy of
+ * the faulting page at correct address.
+ */
+/*** exercise 4.13 ***/
+static void
+pgfault(u_int va)
+{
+	u_int *tmp;
+	//	writef("fork.c:pgfault():\t va:%x\n",va);
+	int perm = ((((Pte *)(*vpt))[VPN(va)]) & 0xfff) | PTE_R;
+	if ((perm & PTE_COW) == 0) {
+		user_panic("fork.c:pgfault():\t va:%x\n",va);
+	}
+	//map the new page at a temporary place
+	perm = perm & (~PTE_COW);
+	if (syscall_mem_alloc(0, USTACKTOP, perm) < 0) {
+		user_panic("fork.c:pgfault() syscall_mem_alloc:\t va:%x\n",va);
+		return;
+	}
+	//copy the content
+	user_bcopy((void *)ROUNDDOWN(va, BY2PG), (void *)USTACKTOP, BY2PG);
+	//map the page on the appropriate place
+	if (syscall_mem_map(0, USTACKTOP, 0, va, perm) < 0) {
+		user_panic("fork.c:pgfault() syscall_mem_map:\t va:%x\n",va);
+		return;
+	}
+	//unmap the temporary place
+	if (syscall_mem_unmap(0, USTACKTOP) < 0) {
+		user_panic("fork.c:pgfault() syscall_mem_unmap:\t va:%x\n",va);
+		return;
+	}
+}
+
+/* Overview:
+ * 	Map our virtual page `pn` (address pn*BY2PG) into the target `envid`
+ * at the same virtual address.
+ *
+ * Post-Condition:
+ *  if the page is writable or copy-on-write, the new mapping must be
+ * created copy on write and then our mapping must be marked
+ * copy on write as well. In another word, both of the new mapping and
+ * our mapping should be copy-on-write if the page is writable or
+ * copy-on-write.
+ *
+ * Hint:
+ * 	PTE_LIBRARY indicates that the page is shared between processes.
+ * A page with PTE_LIBRARY may have PTE_R at the same time. You
+ * should process it correctly.
+ */
+/*** exercise 4.10 ***/
+static void
+duppage(u_int envid, u_int pn)
+{
+	u_int addr;
+	u_int perm;
+
+	addr = pn << PGSHIFT;
+	perm = (((Pte *)(*vpt))[pn]) & 0xfff;
+
+	if ((perm & PTE_R) == 0) {
+		if (syscall_mem_map(0, addr, envid, addr, perm) < 0) {
+			user_panic("duppage not implemented");
+		}
+	} else if ((perm & PTE_COW) != 0) {
+		if (syscall_mem_map(0, addr, envid, addr, perm) < 0) {
+			user_panic("duppage not implemented");
+		}
+	} else if ((perm & PTE_LIBRARY) != 0) {
+		if (syscall_mem_map(0, addr, envid, addr, perm) < 0) {
+			user_panic("duppage not implemented");
+		}
+	} else if ((perm & PTE_R) != 0) {
+		perm = perm | PTE_COW;
+		if (syscall_mem_map(0, addr, envid, addr, perm) < 0) {
+			user_panic("duppage not implemented");
+		}
+		if (syscall_mem_map(0, addr, 0, addr, perm) < 0) {
+			user_panic("duppage not implemented");
+		}
+	}	
+}
+
+/* Overview:
+ * 	User-level fork. Create a child and then copy our address space
+ * and page fault handler setup to the child.
+ *
+ * Hint: use vpd, vpt, and duppage.
+ * Hint: remember to fix "env" in the child process!
+ * Note: `set_pgfault_handler`(user/pgfault.c) is different from
+ *       `syscall_set_pgfault_handler`.
+ */
+/*** exercise 4.9 4.15***/
+extern void __asm_pgfault_handler(void);
+int
+fork(void)
+{
+	// Your code here.
+	u_int newenvid;
+	extern struct Env *envs;
+	extern struct Env *env;
+	u_int i;
+
+	int r;
+	//The parent installs pgfault using set_pgfault_handler
+	set_pgfault_handler(pgfault);
+	//alloc a new alloc
+	newenvid = syscall_env_alloc();
+	if (newenvid == 0) { //child
+		i = syscall_getenvid();
+		env = &envs[ENVX(i)];
+	} else { //parent
+/*		int i, j;
+		Pde *pgdir_fa = (Pde *)(*vpd); // *vpd = (UVPT+(UVPT>>12)*4)  *vpt = UVPT
+		Pte *pgtable_fa;
+		for (i = 0; i < PDX(UTOP); ++i) {
+			if ((pgdir_fa[i] & PTE_V) != 0) {
+				pgtable_fa = ((Pte *)(*vpt)) + (i << 10);
+				for (j = 0; j < PTE2PT; ++j) {
+					if ((pgtable_fa[j] & PTE_V) != 0) {
+						duppage(newenvid, (i << 10) + (j));
+					}
+				}
+			}
+		}*/
+		for (i = 0; i < UTOP -  2 * BY2PG; i += BY2PG) {
+			if ((((Pde *)(*vpd))[i >> PDSHIFT] & PTE_V) && (((Pte *)(*vpt))[i >> PGSHIFT] & PTE_V)) {
+				duppage(newenvid, VPN(i));
+			}
+		}
+		if ((r = syscall_mem_alloc(newenvid, UXSTACKTOP - BY2PG, PTE_V | PTE_R)) < 0) {
+			return r;
+		}
+		if ((r = syscall_set_pgfault_handler(newenvid, __asm_pgfault_handler, UXSTACKTOP)) < 0) {
+			writef("cannot syscall set pgfault handler\n");
+			return r;
+		}
+		if ((r = syscall_set_env_status(newenvid, ENV_RUNNABLE)) < 0) {
+			return r;
+		}
+	}
+
+	return newenvid;
+}
+
+// Challenge!
+int
+sfork(void)
+{
+	user_panic("sfork not implemented");
+	return -E_INVAL;
+}
